61b7b813fbb1bcc7b116d0eef96482c72d8a4a99
anton321223123 <vaincode@mail.ru>
2026-06-07 15:01:49 +0700

    Update готов к выпуску ПРЯМО СЕЙЧАС.py

--- готов к выпуску ПРЯМО СЕЙЧАС.py
+++ готов к выпуску ПРЯМО СЕЙЧАС.py
@@ -7812,5 +7812,6 @@ if __name__ == '__main__':
         print(f"Пароль: admin123")
         print("=" * 50)
 
+    app.run(host='0.0.0.0', port=5000)
     # Строку ниже УДАЛИТЬ или ЗАКОММЕНТИРОВАТЬ
     # app.run(debug=True, port=5000)
