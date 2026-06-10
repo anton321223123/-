e64dc91ab8e4c96d0410359348aef8f50a429ad1
anton321223123 <vaincode@mail.ru>
2026-06-11 02:48:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,6 @@ def health():
     """Простой health check для хостинга"""
     return 'OK', 200
 
-@app.route('/')
-def index_simple():
-    """Временный простой маршрут для проверки"""
-    return 'Zetta server is running!', 200
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
